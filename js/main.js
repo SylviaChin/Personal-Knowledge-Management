fe8770dc2e2e3c6f07cab6cc03511f6ada6c6913
rccoder <rccoder.net@gmail.com>
2015-07-23 20:11:21 +0800

    add database design

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var trueTypeId = -1;
+var allAbstractContent = "AllCate";
+var contentId = -1;
+
+init();
+
+function init() {
+    initDb();
+    initType();
+    initModel();
+}
+
+function initDb() {
+    if ()
+}
